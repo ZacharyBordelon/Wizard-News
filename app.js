b68c355219ec334f801e384a30f3e12cc7fbe121
Zachary Bordelon <zackmbordelon@gmail.com>
2023-03-07 22:56:39 -0600

    First Commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
-const PORT = 1337;
+const {PORT = 1337} = [process.env];
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
